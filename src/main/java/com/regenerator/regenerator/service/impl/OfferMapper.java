8fd5b044c138df0db209ab81f7b6202510323dba
ares <ares@ecwid.com>
2019-04-24 15:08:58 +0400

    add mapper null safe

--- src/main/java/com/regenerator/regenerator/service/impl/OfferMapper.java
+++ src/main/java/com/regenerator/regenerator/service/impl/OfferMapper.java
@@ -4,17 +4,20 @@ import com.regenerator.regenerator.data.yml.Category;
 import com.regenerator.regenerator.data.yml.Offer;
 import com.regenerator.regenerator.data.yml.result.OfferResult;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class OfferMapper {
 
     public static OfferResult toOfferResult(Offer offer, List<Category> categories) {
-        String categoryName = categories.stream()
+        String categoryName = Optional.ofNullable(categories).orElse(Collections.emptyList()).stream()
                 .filter(category -> Objects.equals(category.getId(), offer.getCategoryId()))
                 .map(Category::getValue)
                 .findFirst()
                 .orElse(null);
+
         return new OfferResult().setId(offer.getId())
                 .setAvailable(offer.getAvailable())
                 .setUrl(offer.getUrl())
